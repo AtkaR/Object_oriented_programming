d11d66a027f3b34840594388d456048cc31adcc6
AtkaR <agata-rybak@o2.pl>
2017-07-28 20:03:28 +0200

    28.07

---
+++ src/rybak/agata/WZ_WashingMachine/Device.java
@@ -0,0 +1,8 @@
+package rybak.agata.WZ_WashingMachine;
+
+/**
+ * Created by asus on 2017-06-02.
+ */
+public class Device {
+
+}
---
+++ src/rybak/agata/WZ_WashingMachine/Main.java
@@ -0,0 +1,17 @@
+package rybak.agata.WZ_WashingMachine;
+
+/**
+ * Created by asus on 2017-06-02.
+ */
+public class Main {
+    public static void main(String[] args) {
+        /*Urzadzenie u = new Urzadzenie("Whirpool",3500);
+        Pralka p = new Pralka("Bosh",4000,11,60);
+
+        System.out.println(u);
+        System.out.println(p);
+        System.out.println(u.obliczCena());
+        System.out.println(p.obliczCena());*/
+
+    }
+}
